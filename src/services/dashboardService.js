f6e548f4accaa8379aded025f98943b1f8978cf8
Krishna Sheladiya <ksheladiya9321@gmail.com>
2026-07-29 11:15:53 +0530

    changed dashboard service

--- src/services/dashboardService.js
+++ src/services/dashboardService.js
@@ -1,8 +1,17 @@
 import api from "../api/axiosConfig";
 
 export const getDashboardStats = () => {
-    return api.get("/dashboard/stats");
+    return api.get("/dashboard/stats", {
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`
+        }
+    });
 };
+
 export const getCourseChart = () => {
-    return api.get("/dashboard/course-chart");
+    return api.get("/dashboard/course-chart", {
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`
+        }
+    });
 };
